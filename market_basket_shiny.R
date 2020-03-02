78693b610b53ae8bd695b5445fd339d1059a6e7d
pjdoucet <57510802+pjdoucet@users.noreply.github.com>
2020-03-01 19:13:04 -0500

    Updated Shiny App Sunday March 1st

---
+++ market_basket_shiny.R
@@ -0,0 +1,148 @@
+# Load Libraries
+library(knitr)
+# library(ggplot2)
+# library(lubridate)
+library(arules)
+library(arulesViz)
+library('dplyr')
+library('data.table')
+library(shinydashboard)
+library(shiny)
+
+# Load files needed for selecting products
+#aisles = read.csv("./input/aisles.csv")
+departments = read.csv("./input/departments.csv")
+products = read.csv("./input/products.csv")
+products_for_dept <- subset(products, department_id %in% 1)
+head(products)
+head(products_for_dept)
+products_in_cart <- data.frame(product_id=integer(), product_name=character(), aisle_id=integer(), department_id=integer()) 
+# products_in_cart <- products_for_dept[0,]
+
+# Load Transaction data
+suppressWarnings(
+    tr <- read.transactions('./input/InstaCart_MBA.csv', format = 'basket', sep=',')
+)
+
+grocery_item = "Garlic"
+rules <- apriori(tr, parameter = list(supp=0.001, conf=0.1),
+                 appearance = list(default="rhs", lhs=grocery_item),
+                 control = list (verbose=F))
+saveRDS(rules,file="./rules.rds")
+myRules <<- readRDS("./rules.rds")
+#
+# rules_subset <- subset(rules, subset = lhs %in% grocery_item)
+# rules_conf <- sort (rules_subset, by="confidence", decreasing=TRUE) # 'high-confidence' rules.
+# result = inspect(head(rules_conf))
+# result$rhs
+#
+#
+# rules_conf <- sort (rules, by="confidence", decreasing=TRUE) # 'high-confidence' rules.
+# #
+# result = inspect(head(rules_conf))
+# result$rhs
+
+
+
+
+
+# Define UI
+ui <- dashboardPage(
+  dashboardHeader(title = "Group 8 Market"),
+  dashboardSidebar(
+    selectInput('dept_col', 'Department', departments$department)
+    #selectInput('aisle_col', 'Aisle', aisles$aisle)
+  ),
+  dashboardBody(title = "Group 8 Market", 
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(
+        textOutput(outputId = "dept_val"),
+        textOutput(outputId = "last_added"),
+      ),
+      actionButton("add_to_cart", "Add to Cart"),
+      actionButton("clear_cart", "Clear Cart") 
+    ),
+    fluidRow(
+      box(
+        title = "Products:",
+        selectInput('prod_col', NULL, products_for_dept$product_name, size = 10, selectize = FALSE),
+      ),
+      box(
+        title = "Cart:",
+        selectInput('cart_col', NULL, products_in_cart$product_name, size = 10, selectize = FALSE),
+      )
+    ),
+    fluidRow(
+      box(
+        title = "Product Recomendations:", width=12,
+        tableOutput(outputId = "product_added_to_cart"),
+      )
+    )
+
+  )
+)
+
+# Define server function
+server <- function(input, output, session) {
+  
+      session$userData$products_in_cart <- products_in_cart[0,]
+      updateSelectInput(session=session, inputId = "cart_col",
+                          label = NULL,
+                          choices = session$userData$products_in_cart$product_name,
+                          selected = NULL
+        )
+
+      output$dept_val <- renderText({
+        sel_dept_row = which(departments$department == input$dept_col)
+        session$userData$products_for_dept <<- subset(products, department_id %in% departments$department_id[sel_dept_row])
+        updateSelectInput(session, "prod_col",
+                          label = NULL,
+                          choices = session$userData$products_for_dept$product_name,
+                          selected = NULL
+        )
+        paste("Dept:", input$dept_col, sep=" ")
+        })
+    
+      recommend_update <- function()
+        {
+        output$product_added_to_cart <- renderTable({
+          
+          rules_subset <- subset(rules, subset = lhs %in% as.character(input$dept_col))#grocery_item)
+          rules_conf <- sort (rules_subset, by="confidence", decreasing=TRUE) # 'high-confidence' rules.
+          result = inspect(head(rules_conf))
+          if(is.null(result$rhs)){
+            result$rhs <- "NA"
+          }
+          result$rhs
+          }, rownames=FALSE, colnames=FALSE)
+      }
+        
+      observeEvent(input$clear_cart, {
+        session$userData$products_in_cart <<- products_in_cart[0,]
+        updateSelectInput(session=session, inputId = "cart_col",
+                          label = NULL,
+                          choices = session$userData$products_in_cart$product_name,
+                          selected = NULL
+         )
+      })
+      
+
+      observeEvent(input$add_to_cart, {
+        prod_row = which(session$userData$products_for_dept$product_name == input$prod_col)
+        session$userData$products_in_cart <<- rbind(session$userData$products_in_cart, session$userData$products_for_dept[prod_row,])
+        updateSelectInput(session, "cart_col",
+                          label = NULL,
+                          choices = session$userData$products_in_cart$product_name,
+                          selected = NULL
+        )
+        recommend_update()
+      })
+      
+
+}
+
+# Run Shiny App
+shinyApp(ui = ui, server = server)
+
+
